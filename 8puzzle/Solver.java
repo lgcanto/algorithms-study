2f75d42b31414eef39d9acbe5bf004f42147d81c
Lucas Canto <lucas.canto@siemens.com>
2020-06-20 21:03:08 -0300

    Removing TODO tag

--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -71,7 +71,7 @@ public class Solver {
                 }
                 twinPreviousSearchNode = twinDeletedSearchNode;
             }
-            // TODO: remove hack
+            // remove hack
             if (movesHolder > initial.dimension()*initial.dimension()) {
                 twinSolutionFound = true;
                 break;
